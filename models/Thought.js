5d57997cf278d00c02897fcfcfce21d08a7cf8ea
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-04 14:35:23 -0400

    Create Thought.js

---
+++ models/Thought.js
@@ -0,0 +1,38 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minLength: 1,
+      maxLength: 280,
+    },
+    createdAt: {
+      type: date,
+      default: Date.now(),
+      // TODO: Use a getter method to format the timestamp on query
+    },
+    // The user that created this thought
+    username: {
+      type: String,
+      required: true,
+    },
+    // These are like replies
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+// TODO: Create a virtual called reactionCount that retrieves the length of the thought's reactions array field on query.
+
+// Initialize User model
+const Thought = model("thought", thoughtSchema);
+
+module.exports = Thought;
