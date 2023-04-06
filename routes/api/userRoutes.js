05a056340b0c72864f4936d5fade88e58fc9cd1a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-06 14:51:53 -0400

    Add routes for getUsers, createUser, getSingleUser, deleteUser

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -1,15 +1,23 @@
 const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser,
+} = require('../../controllers/userControllers');
 
 // /api/users
-// GET all users
-// GET a single user by its _id and populated thought and friend data
-// POST a new user:
-// PUT to update a user by its _id
-// DELETE to remove user by its _id
+router.route('/').get(getUsers).post(createUser);
+
+// /api/users/:userId
+router.route('/:userId').get(getSingleUser).delete(deleteUser);
+
+// TODO: PUT to update a user by its _id
+// TODO: DELETE to remove user by its _id
 // BONUS: Remove a user's associated thoughts when deleted
 
 // /api/users/:userId/friends/:friendId
-// POST to add a new friend to a user's friend list
-// DELETE to remove a friend from a user's friend list
+// TODO: POST to add a new friend to a user's friend list
+// TODO: DELETE to remove a friend from a user's friend list
 
 module.exports = router;
