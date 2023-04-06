fc011ec6e89b44d9a8c12c272bc879b514ee806a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-06 14:52:19 -0400

    Add routes for getThoughts, getSingleThought

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -1,14 +1,21 @@
 const router = require('express').Router();
+const {
+  getThoughts,
+  getSingleThought,
+} = require('../../controllers/thoughtControllers');
 
 // /api/thoughts
-// GET to get all thoughts
-// GET to get a single thought by its _id
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-// PUT to update a thought by its _id
-// DELETE to remove a thought by its _id
+router.route('/').get(getThoughts);
+
+// /api/thoughts/:thoughtId
+router.route('/:thoughtId').get(getSingleThought);
+
+// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// TODO: PUT to update a thought by its _id
+// TODO: DELETE to remove a thought by its _id
 
 // /api/thoughts/:thoughtId/reactions
-// POST to create a reaction stored in a single thought's reactions array field
-// DELETE to pull and remove a reaction by the reaction's reactionId value
+// TODO: POST to create a reaction stored in a single thought's reactions array field
+// TODO: DELETE to pull and remove a reaction by the reaction's reactionId value
 
 module.exports = router;
