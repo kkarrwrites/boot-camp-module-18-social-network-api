706e4b6ecc99e30179993aec15240311f817981a
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-06 14:51:25 -0400

    Add getUsers, getSingleUser, createUser, deleteUser

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,15 +1,58 @@
 const { User, Thought, Reaction } = require('../models');
+const { modelName } = require('../models/User');
 
 // /api/users
-// GET all users
-// GET a single user by its _id and populated thought and friend data
-// POST a new user:
-// PUT to update a user by its _id
-// DELETE to remove user by its _id
-// BONUS: Remove a user's associated thoughts when deleted
+module.exports = {
+  // GET all users
+  getUsers(req, res) {
+    User.find()
+      .then((users) => res.json(users))
+      .catch((err) => res.status(500).json(err));
+  },
+  // GET a single user by its _id
+  // TODO: populated thought and friend data
+  getSingleUser(req, res) {
+    User.findOne({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(400).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+  // POST a new user
+  createUser(req, res) {
+    User.create(req.body)
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.status(500).json(err));
+  },
+  // TODO: PUT to update a user by its _id
+  // DELETE to remove user by its _id
+  deleteUser(req, res) {
+    User.findOneAndRemove({ _id: req.params.userId })
+      .then((user) =>
+        !user
+          ? res.status(404).json({ message: 'No such user exists' })
+          : User.findOneAndUpdate(
+              { users: req.params.userId },
+              { $pull: { users: req.parms.userId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'User created, but no user with this id',
+            })
+          : res.json({ message: 'User deleted' })
+      )
+      .catch((err) => res.status(500).json(err));
+  },
+  // BONUS: Remove a user's associated thoughts when deleted
+};
 
 // /api/users/:userId/friends/:friendId
-// POST to add a new friend to a user's friend list
-// DELETE to remove a friend from a user's friend list
-
-module.exports = {};
+// TODO: POST to add a new friend to a user's friend list
+// TODO: DELETE to remove a friend from a user's friend list
