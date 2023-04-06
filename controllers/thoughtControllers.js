53db59b4cbcd68bca5dfb8c48c3b40807c47e184
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-06 14:52:06 -0400

    Add getThoughts, getSingleThought

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,14 +1,31 @@
 const { User, Thought, Reaction } = require('../models');
 
 // /api/thoughts
-// GET to get all thoughts
-// GET to get a single thought by its _id
-// POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-// PUT to update a thought by its _id
-// DELETE to remove a thought by its _id
+module.exports = {
+  // GET to get all thoughts
+  getThoughts(req, res) {
+    Thought.find()
+      .then((thoughts) => res.json(thoughts))
+      .catch((err) => res.status(500).json(err));
+  },
+  // GET to get a single thought by its _id
+  getSingleThought(req, res) {
+    Thought.findOne({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(400).json({ message: 'No thought with that ID' })
+          : res.json(thought)
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
+};
+
+// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+// TODO: PUT to update a thought by its _id
+// TODO: DELETE to remove a thought by its _id
 
 // /api/thoughts/:thoughtId/reactions
-// POST to create a reaction stored in a single thought's reactions array field
-// DELETE to pull and remove a reaction by the reaction's reactionId value
-
-module.exports = {};
+// TODO: POST to create a reaction stored in a single thought's reactions array field
+// TODO: DELETE to pull and remove a reaction by the reaction's reactionId value
