b36db58faa8f7621b1abc33782d068d037dfa2ed
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-07 11:01:50 -0400

    Add delete(deleteThought) to /:thoughtId route

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,17 +2,16 @@ const router = require('express').Router();
 const {
   getThoughts,
   getSingleThought,
+  deleteThought,
 } = require('../../controllers/thoughtControllers');
 
 // /api/thoughts
 router.route('/').get(getThoughts);
+// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
 
 // /api/thoughts/:thoughtId
-router.route('/:thoughtId').get(getSingleThought);
-
-// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.route('/:thoughtId').get(getSingleThought).delete(deleteThought);
 // TODO: PUT to update a thought by its _id
-// TODO: DELETE to remove a thought by its _id
 
 // /api/thoughts/:thoughtId/reactions
 // TODO: POST to create a reaction stored in a single thought's reactions array field
