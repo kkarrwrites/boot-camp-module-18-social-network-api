d6686b116c07525530277a4ab2387d81598ea0b1
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-07 11:01:31 -0400

    Update userControllers.js
    Add populate('thoughts') and populate('friends') to getSingleUser
    
    Add PUT for updateUser
    
    Edit DELETE for deleteUser

--- controllers/userControllers.js
+++ controllers/userControllers.js
@@ -1,7 +1,6 @@
 const { ObjectId } = require('mongoose').Types;
 const { User } = require('../models');
 
-// /api/users
 module.exports = {
   // GET all users
   getUsers(req, res) {
@@ -9,9 +8,11 @@ module.exports = {
       .then((users) => res.json(users))
       .catch((err) => res.status(500).json(err));
   },
-  // GET a single user by its _id and TODO: populated thought and friend data
+  // GET a single user by its _id and populate thought and friend data
   getSingleUser(req, res) {
     User.findOne({ _id: req.params.userId })
+      .populate('thoughts')
+      .populate('friends')
       .then((user) =>
         !user
           ? res.status(400).json({ message: 'No user with that ID' })
@@ -24,34 +25,43 @@ module.exports = {
   // POST a new user
   createUser(req, res) {
     User.create(req.body)
-      .then((dbUserData) => res.json(dbUserData))
+      .then((user) => res.json(user))
       .catch((err) => res.status(500).json(err));
   },
-  // TODO: PUT to update a user by its _id
+  // PUT to update a user by its _id
+  updateUser(req, res) {
+    User.findOneAndUpdate(
+      // Filter
+      { _id: req.params.userId },
+      // Update
+      // $set operator replaces the value of a field with the specified value
+      { $set: req.body },
+      // { run:Validators: true } makes sure that the data added to the database fits the parameters of the database
+      // { new: true } returns the document after update was applied because by default findOneAndUpdate() returns the document as it was before update was applied
+      { runValidators: true, new: true }
+    )
+      .then((user) =>
+        !user
+          ? res.status(400).json({ message: 'No user with that ID' })
+          : res.json(user)
+      )
+      .catch((err) => {
+        res.status(500).json(err);
+      });
+  },
   // DELETE to remove user by its _id
-  // BONUS: Remove a user's associated thoughts when deleted
+  // TODO: BONUS Remove a user's associated thoughts when deleted
   deleteUser(req, res) {
-    User.findOneAndRemove({ _id: req.params.userId })
-      .then((user) =>
+    User.findOneAndDelete({ _id: req.params.userId })
+      .then((user) => {
         !user
-          ? res.status(404).json({ message: 'No such user exists' })
-          : User.findOneAndUpdate(
-              { users: req.params.userId },
-              { $pull: { users: req.parms.userId } },
-              { new: true }
-            )
-      )
-      .then((user) =>
-        !user
-          ? res.status(404).json({
-              message: 'User created, but no user with this id',
-            })
-          : res.json({ message: 'User deleted' })
-      )
-      .catch((err) => res.status(500).json(err));
+          ? res.status(400).json({ message: 'No user with that ID' })
+          : res.json({ message: 'User was deleted' });
+      })
+      .catch((err) => {
+        res.status(500).json(err);
+      });
   },
+  // TODO: POST to add a new friend to a user's friend list
+  // TODO: DELETE to remove a friend from a user's friend list
 };
-
-// /api/users/:userId/friends/:friendId
-// TODO: POST to add a new friend to a user's friend list
-// TODO: DELETE to remove a friend from a user's friend list
