34fd16c1910e376f170c597cbed85edbdd88571f
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-07 11:02:05 -0400

    Add deleteThought

--- controllers/thoughtControllers.js
+++ controllers/thoughtControllers.js
@@ -1,7 +1,6 @@
 const { ObjectId } = require('mongoose').Types;
-const { Thought } = require('../models');
+const { Thought, User } = require('../models');
 
-// /api/thoughts
 module.exports = {
   // GET to get all thoughts
   getThoughts(req, res) {
@@ -21,12 +20,32 @@ module.exports = {
         res.status(500).json(err);
       });
   },
+  // TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+  // TODO: PUT to update a thought by its _id
+  // DELETE to remove a thought by its _id
+  deleteThought(req, res) {
+    Thought.findOneAndRemove({ _id: req.params.thoughtId })
+      .then((thought) =>
+        !thought
+          ? res.status(404).json({ message: 'No such thought exists' })
+          : User.findOneAndUpdate(
+              { thoughts: req.params.thoughtId },
+              { $pull: { thoughts: req.params.thoughtId } },
+              { new: true }
+            )
+      )
+      .then((user) =>
+        !user
+          ? res.status(404).json({
+              message: 'Thought deleted, but no user with this ID found',
+            })
+          : res.json({ message: 'Thought successfully deleted' })
+      )
+      .catch((err) => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  },
+  // TODO: POST to create a reaction stored in a single thought's reactions array field
+  // TODO: DELETE to pull and remove a reaction by the reaction's reactionId value
 };
-
-// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
-// TODO: PUT to update a thought by its _id
-// TODO: DELETE to remove a thought by its _id
-
-// /api/thoughts/:thoughtId/reactions
-// TODO: POST to create a reaction stored in a single thought's reactions array field
-// TODO: DELETE to pull and remove a reaction by the reaction's reactionId value
