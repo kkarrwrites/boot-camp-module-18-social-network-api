db6ec9901f5590ebe56a820c776c4bfd3cd4cfac
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-09 17:13:03 -0400

    Add post(createThought)

--- routes/api/thoughtRoutes.js
+++ routes/api/thoughtRoutes.js
@@ -2,13 +2,13 @@ const router = require('express').Router();
 const {
   getThoughts,
   getSingleThought,
+  createThought,
   updateThought,
   deleteThought,
 } = require('../../controllers/thoughtControllers');
 
 // /api/thoughts
-router.route('/').get(getThoughts);
-// TODO: POST to create a new thought (don't forget to push the created thought's _id to the associated user's thoughts array field)
+router.route('/').get(getThoughts).post(createThought);
 
 // /api/thoughts/:thoughtId
 router
