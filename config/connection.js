207f3eaf65b2ca18d3343915ce2da4b62ebdc727
kkarrwrites <57644505+kkarrwrites@users.noreply.github.com>
2023-04-10 18:18:49 -0400

    Change connection.js to be like mini project example

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,9 @@
 const { connect, connection } = require('mongoose');
 
-// Wrap Mongoose around local connection to MongoDB
-connect('mongodb://127.0.0.1:27017/socialNetworkDB', {
+const connectionString =
+  process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/socialNetworkDB';
+
+connect(connectionString, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
